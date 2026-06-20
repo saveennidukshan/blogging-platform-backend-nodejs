5476f905a5f577b7a115e1e17867dfce7bd3f09b
Saveen <saveennidukshan@gmail.com>
2026-06-20 15:47:19 +0530

    some changes

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,33 +1,31 @@
-import { createUser, getUserByEmail } from "./auth.model.js";
+import { createUser, getUserByEmail } from './auth.model.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-
 export const register = async (req, res) => {
-    try{
-        const {email,password} = req.body;
-        const user = await getUserByEmail(email);
-        if(user) return res.send("alredy created")
-        const hash = bcrypt.hashSync(password, 10);
-        await createUser(email, hash);
-        res.send("User created")
-
-    }catch(e){
-        console.log(e);   
-        return res.send("cred error")
-    }
+  try {
+    const { email, password } = req.body;
+    const user = await getUserByEmail(email);
+    if (user) return res.send('alredy created');
+    const hash = await bcrypt.hash(password, 10);
+    await createUser(email, hash);
+    res.send('User created');
+  } catch (e) {
+    console.log(e);
+    return res.send('cred error');
+  }
 };
 
 export const login = async (req, res) => {
-    try{
-        const {email,password} = req.body;
-        const user = await getUserByEmail(email);
-        if(!user) return res.send("user not found")
-        if(!bcrypt.compareSync(password, user.password)) return res.send("wrong password");
-        const token = jwt.sign({ email }, 'myTojwen43', {expiresIn: '1h'});
-        res.json({token})
-    }catch(e){
-        console.log(e);   
-        return res.send("cred error")
-    }
+  try {
+    const { email, password } = req.body;
+    const user = await getUserByEmail(email);
+    if (!user) return res.send('user not found');
+    if (!bcrypt.compareSync(password, user.password)) return res.send('wrong password');
+    const token = jwt.sign({ email }, 'myTojwen43', { expiresIn: '1h' });
+    res.json({ token });
+  } catch (e) {
+    console.log(e);
+    return res.send('cred error');
+  }
 };
--- src/modules/auth/auth.model.js
+++ src/modules/auth/auth.model.js
@@ -1,20 +1,20 @@
-import { prisma } from "./../../configs/db.js"
+import { prisma } from './../../configs/db.js';
 
-export const createUser = async(email, password)=>{
-    const user = await prisma.user.create({
-        data:{
-            email,
-            password
-        }
-    })
-    return user;
-}
+export const createUser = async (email, password) => {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password,
+    },
+  });
+  return user;
+};
 
-export const getUserByEmail = async(email)=>{
-    const user = await prisma.user.findUnique({
-        where:{
-            email
-        }
-    })
-    return user;
-}
+export const getUserByEmail = async (email) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+};
